ec94f6dade6ff8eb72b221cac6a144b8941741e4
alejandrog1028 <thealejgarcia@gmail.com>
2025-06-24 21:43:47 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("resizeButton").addEventListener("click", function (){
+    document.getElementById("dogImage").style.width = "500px";
+    document.getElementById("dogImage").style.border = "20px groove blue";
+});
